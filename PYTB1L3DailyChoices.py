0d5f16481001bc5d641603d009357ce36616e1f8
Davey van Wijk <30650@ma-web.nl>
2020-09-23 14:59:30 +0200

    PYTB1L3DailyChoices

---
+++ PYTB1L3DailyChoices.py
@@ -0,0 +1,12 @@
+print("je alarm gaat af, wat doe je. SNOOZE, OPSTAAN, ALARM UIT ZETTEN of YOUTUBE KIJKEN")
+keuze = input()
+if keuze == 'SNOOZE':
+	print("als je gaat rushen haal je school")
+elif keuze == 'OPSTAAN':
+	print("je haalt school maar je bent veelste vroeg")
+elif keuze == 'ALARM UIT ZETTEN':
+	print("je haalt school niet omdat je 2 uur bent door geslapen")
+elif keuze == 'YOUTUBE KIJKEN':
+	print("je komt telaat op school en bent absent gemelt")
+else:
+	print("keuze niet geldig")
