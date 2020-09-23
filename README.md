e27f50ad3722854e6f2be42826bb33ae42f51af6
Davey van Wijk <30650@ma-web.nl>
2020-09-23 15:02:44 +0200

    tweede commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# PYTB1L1PlayWIthPython-" 
 "# PYTB1L2PoemPrinting" 
 "# PYTB1L2PoemPrinting" 
+"# PYTB1L3DailyChoices.py" 
---
+++ varA.py
@@ -0,0 +1,16 @@
+varA = 5
+varB = 10
+if ( varA == 5 ):
+   print ("varA staat gelijk aan 5")
+   print("extra tab")
+else : 
+    print("varA staat niet gelijk aan 5")
+varA = 11
+if ( varA == varB ) :
+   print("varA staat gelijk aan varB")
+elif ( varA < varB ) :
+   print("varA is kleiner dan varB")
+else :
+   print("dan is varA groter dan VarB")
+
+print("einde programma")
